acf4e377b63216185382a3292e4578ff58fe3258
alicia <aliciafdezrov@gmail.com>
2019-11-29 13:18:33 +0100

    Added new components and styles to existing components

--- src/students.tsx
+++ src/students.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import { AverageComponent } from "./averageComponent";
 const logoImg = require("./content/logo_1.png");
 
+
+const img = document.createElement("img");
+img.src = logoImg;
+document.getElementById("imgContainer").appendChild(img);
+
 ReactDOM.render(
     <div>
         <h1>Hello from React DOM</h1>
@@ -10,6 +15,3 @@ ReactDOM.render(
     </div>,
     document.getElementById("root")
 );
-const img = document.createElement("img");
-img.src = logoImg;
-document.getElementById("imgContainer").appendChild(img);
